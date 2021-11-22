0cdfb749edd6957082a2c6c2e788ef37e31d6192
SujanChaguthi <86692329+SujanChaguthi@users.noreply.github.com>
2021-11-21 19:54:32 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-2_uP0sBYuQp', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '3ta61htltbjugf96rirlq2khu', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
